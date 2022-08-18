dbc0e7c35e3d6613cb7513761b9b474f95130ca8
liuxq <xqliu@relaper.com>
2022-08-18 16:56:07 +0800

    feat: init

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index.ts'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]-[contenthash:8].bundle.js'
+  },
+  devServer: {
+    port: 3000,
+    hot: true,
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html')
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+}
