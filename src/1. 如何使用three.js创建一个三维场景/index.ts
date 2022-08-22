01a58a405c620c6f8cac5327b82db5a9ab55c64e
向阳 <xiangqian_liu@163.com>
2022-08-22 15:47:05 +0800

    update

--- src/1. 如何使用three.js创建一个三维场景/index.ts
+++ src/1. 如何使用three.js创建一个三维场景/index.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-
+import { GUI } from 'dat.gui'
 // 创建一个场景
 const scene = new THREE.Scene()
 // 创建一个相机
@@ -19,6 +19,8 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setClearColor(new THREE.Color(0x000000))
 // 指定渲染器宽高
 renderer.setSize(window.innerWidth, window.innerHeight)
+// 启用阴影
+renderer.shadowMap.enabled = true
 
 // 创建一个坐标系，大小为20
 const axes = new THREE.AxesHelper(20)
@@ -34,6 +36,9 @@ const planeMaterial = new THREE.MeshBasicMaterial({
 })
 // 通过Mesh，将形状和材质组合在一起
 const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+
+plane.receiveShadow = true
+
 // 将平面绕x轴旋转-90度
 plane.rotation.x = -0.5 * Math.PI
 // 设置平面在场景中的位置
@@ -43,12 +48,13 @@ scene.add(plane)
 
 // 创建一个球体
 // 半径为4，x方向的细分数为20，y方向的细分数为20，细分数越大表示经纬线越密集 
-const sphereGeometry = new THREE.SphereGeometry(4, 20, 20)
+const sphereGeometry = new THREE.SphereGeometry(4, 15, 15)
 const sphereMaterial = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
+  color: 0x009990,
   wireframe: true
 })
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+sphere.castShadow = true
 sphere.position.set(20, 4, 0)
 scene.add(sphere)
 
@@ -62,4 +68,32 @@ camera.lookAt(scene.position)
 const container = document.getElementById('app')!
 // 将渲染的内容插入到页面中
 container.appendChild(renderer.domElement)
-renderer.render(scene, camera)
+
+const controls = {
+  bouncingSpeed: 0.04
+}
+const gui = new GUI()
+gui.add(controls, 'bouncingSpeed', 0, 0.5)
+
+// 让小球跳起来
+const MaxHeight = 10
+let step = 0.08
+let timer: number
+function renderScene() {
+  if (timer) {
+    cancelAnimationFrame(timer)
+  }
+  sphere.position.y += controls.bouncingSpeed
+  if (sphere.position.y > MaxHeight) {
+    sphere.position.y = MaxHeight
+    controls.bouncingSpeed = -controls.bouncingSpeed
+  }
+  if (sphere.position.y < 4) {
+    sphere.position.y = 4
+    controls.bouncingSpeed = -controls.bouncingSpeed
+  }
+  timer = requestAnimationFrame(renderScene)
+  renderer.render(scene, camera)  
+}
+
+renderScene()
